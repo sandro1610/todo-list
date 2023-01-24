88a9b5c7d77733e5ac4991d24723a07bd4757841
Mikhael Sandro <mikhaelsandro82@gmail.com>
2023-01-24 15:00:37 +0700

    if else

--- controllers/ActivitiesController.js
+++ controllers/ActivitiesController.js
@@ -18,17 +18,16 @@ export const getActivityById = async (req, res) => {
         const id = req.params.id
         const activity = await Activities.findOne({where: {id: id}});
         if (activity == null ) {
-            res.status(404).json({
+            return res.status(404).json({
                 "status": "Not Found",
                 "message": `Activity with ID ${id} Not Found`
             });
-        }else {
-            res.status(200).json({
-                "status": "Success",
-                "message": "Success",
-                "data": activity
-            })
-        };
+        }
+        res.status(200).json({
+            "status": "Success",
+            "message": "Success",
+            "data": activity
+        })
     } catch (error) {
         res.status(500).json(error);
     }
@@ -38,22 +37,21 @@ export const createActivity = async (req, res) => {
     try {
         const title = req.body.title;
         const email = req.body.email;
-        if (title == null || title == undefined) {
-            res.status(400).json({
+        if (title == null) {
+            return res.status(400).json({
                 "status": "bad Request",
                 "message": "title cannot be null"
             });
-        } else {
-            const createActivity = await Activities.create({
-                title : title,
-                email: email
-            });
-            res.status(201).json({
-                "status": "Success",
-                "message": "Success",
-                "data": createActivity
-            });
-        }
+        } 
+        const createActivity = await Activities.create({
+            title : title,
+            email: email
+        });
+        res.status(201).json({
+            "status": "Success",
+            "message": "Success",
+            "data": createActivity
+        });
     } catch (error) {
         res.status(500).json(error);
     }
@@ -64,43 +62,41 @@ export const updateActivity = async (req, res) => {
     const title = req.body.title;
     const activity = await Activities.findOne({where: {id: id}});
     if (activity == null) {
-        res.status(404).json({
+        return res.status(404).json({
             "status": "Not Found",
             "message": `Activity with ID ${id} Not Found`
         });
     } else if (title == null || title == undefined) {
-        res.status(400).json({
+        return res.status(400).json({
             "status": "Bad Request",
             "message": "Title cannot be null"
         });
-    } else {
-        await Activities.update({
-            title : title
-        },{
-            where: {id: id}
-        });
-        const activityUpdate = await Activities.findOne({where: {id: id}});
-        res.status(200).json({
-            "status": "Success",
-            "message": "Success",
-            "data": activityUpdate
-        });
     }
+    await Activities.update({
+        title : title
+    },{
+        where: {id: id}
+    });
+    const activityUpdate = await Activities.findOne({where: {id: id}});
+    res.status(200).json({
+        "status": "Success",
+        "message": "Success",
+        "data": activityUpdate
+    });
 }
 
 export const deleteActivity = async (req, res) => {
     const id = req.params.id
     const activity = await Activities.findOne({where: {id: id}});
     if (activity == null ) {
-        res.status(404).json({
+        return res.status(404).json({
             "status": "Not Found",
             "message": `Activity with ID ${id} Not Found`
         });
-    }else {
-        await Activities.destroy({where: {id: id}});
-        res.status(200).json({
-            "status": "Success",
-            "message": "Success"
-        })
-    };
+    }
+    await Activities.destroy({where: {id: id}});
+    res.status(200).json({
+        "status": "Success",
+        "message": "Success"
+    })
 }
--- controllers/TodosController.js
+++ controllers/TodosController.js
@@ -24,17 +24,16 @@ export const getTodoById = async (req, res) => {
         const id = req.params.id
         const todo = await Todos.findOne({where: {id: id}});
         if (todo == null ) {
-            res.status(404).json({
+            return res.status(404).json({
                 "status": "Not Found",
                 "message": `Todo with ID ${id} Not Found`
             });
-        }else {
-            res.status(200).json({
-                "status": "Success",
-                "message": "Success",
-                "data": todo
-            })
-        };
+        }
+        res.status(200).json({
+            "status": "Success",
+            "message": "Success",
+            "data": todo
+        })
     } catch (error) {
         res.status(500).json(error);
     }
@@ -45,28 +44,27 @@ export const createTodo = async (req, res) => {
         const title = req.body.title;
         const activity_group_id = req.body.activity_group_id;
         const priority = req.body.priority;
-        if (title == null ) {
-            res.status(400).json({
+        if (title == null) {
+            return res.status(400).json({
                 "status": "Bad Request",
                 "message": "title cannot be null"
             });
-        } else if (activity_group_id == null || activity_group_id == undefined) {
-            res.status(400).json({
+        } else if (activity_group_id == null) {
+            return res.status(400).json({
                 "status": "Bad Request",
                 "message": "activity_group_id cannot be null"
             });
-        } else {
-            const todoCreate = await Todos.create({
-                title : title,
-                activity_group_id : activity_group_id,
-                priority : priority
-            });
-            res.status(201).json({
-                "status": "Success",
-                "message": "Success",
-                "data": todoCreate
-            });
         }
+        const todoCreate = await Todos.create({
+            title : title,
+            activity_group_id : activity_group_id,
+            priority : priority
+        });
+        res.status(201).json({
+            "status": "Success",
+            "message": "Success",
+            "data": todoCreate
+        });
     } catch (error) {
         res.status(500).json(error);
     }
@@ -77,43 +75,41 @@ export const updateTodo = async (req, res) => {
     const title = req.body.title;
     const todo = await Todos.findOne({where: {id: id}});
     if (todo == null) {
-        res.status(404).json({
+        return res.status(404).json({
             "status": "Not Found",
             "message": `Todo with ID ${id} Not Found`
         });
     } else if (title == null || title == undefined) {
-        res.status(400).json({
+        return res.status(400).json({
             "status": "Bad Request",
             "message": "Title cannot be null"
         });
-    } else {
-        await Todos.update({
-            title : title
-        },{
-            where: {id: id}
-        });
-        const todoUpdate = await Todos.findOne({where: {id: id}});
-        res.status(200).json({
-            "status": "Success",
-            "message": "Success",
-            "data": todoUpdate
-        });
     }
+    await Todos.update({
+        title : title
+    },{
+        where: {id: id}
+    });
+    const todoUpdate = await Todos.findOne({where: {id: id}});
+    res.status(200).json({
+        "status": "Success",
+        "message": "Success",
+        "data": todoUpdate
+    });
 }
 
 export const deleteTodo = async (req, res) => {
     const id = req.params.id
     const todo = await Todos.findOne({where: {id: id}});
     if (todo == null) {
-        res.status(404).json({
+        return res.status(404).json({
             "status": "Not Found",
             "message": `Todo with ID ${id} Not Found`
         });
-    }else {
-        await Todos.destroy({where: {id: id}});
-        res.status(200).json({
-            "status": "Success",
-            "message": "Success"
-        })
-    };
+    }
+    await Todos.destroy({where: {id: id}});
+    res.status(200).json({
+        "status": "Success",
+        "message": "Success"
+    });
 }
